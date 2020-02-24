984f222d73b85a88974e3f7b805ae7b372a7af21
Harun <harun.memis@fabrikamedya.com.tr>
2020-02-24 11:44:09 +0300

    make it better

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const app = express();
+var cors = require("cors");
+
+const port = 8080;
+
+// var request = require("request"),
+
+var cloudscraper = require("cloudscraper");
+app.use(cors());
+
+const API_URL = "https://api.sikayetvar.com";
+
+const username = "harunn.memis@gmail.com";
+const password = "hrn1234";
+
+app.post("/auth-member/auth/login", (req, res) => {
+  const data = {
+    grant_type: "password",
+    client_id: "sikayetvar",
+    username,
+    password
+  };
+
+  var options = {
+    uri: API_URL + "/auth-member/auth/login",
+    formData: data
+  };
+
+  cloudscraper
+    .post(options)
+    .then(data => res.json(JSON.parse(data)))
+    .catch(e => {
+      console.error(e);
+      res.statusCode = 400;
+      res.end();
+    });
+});
+app.get("/core/me", (req, res) => {
+  const Authorization = req.get("Authorization");
+
+  cloudscraper
+    .get({
+      uri: API_URL + "/core/me",
+      headers: {
+        Authorization
+      }
+    })
+    .then(data => res.json(JSON.parse(data)))
+    .catch(e => {
+      console.error(e);
+      res.statusCode = 400;
+      res.end();
+    });
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
